0f9e1675610bfc78f01c7288dcff5983aa34a2ba
marco <marcoaurelio1655@ciencias.unam.mx>
2016-10-18 19:07:00 -0500

    pagina de inicio puesta

--- Manual2/app/src/main/java/com/example/marco/manual2/MainActivity.java
+++ Manual2/app/src/main/java/com/example/marco/manual2/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.marco.manual2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class MainActivity extends AppCompatActivity {
     private WebView mWebView;
@@ -14,10 +16,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mWebView = (WebView) findViewById(R.id.vista);
+        mWebView.setWebViewClient(new WebViewClient());
         mWebView.addJavascriptInterface(new WebAppInterface(this), "Android");
         WebSettings webSettings = mWebView.getSettings();
         webSettings.setJavaScriptEnabled(true);
-        mWebView.loadUrl("file:///android_asset/www/formulario.html");
+        mWebView.loadUrl("file:///android_asset/www/inicio.html");
 
     }
 }
